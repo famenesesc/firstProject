e2c54e792376b8a6bf251b24e50946e0bf8b0c99
Essam101 <essamsaleh101@gmail.com>
2020-08-23 19:58:32 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,28 +1,2 @@
-// !! IMPORTANT README:
-
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
-
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
 "use strict";
+var t, r = document.getElementById("image"), a = e(r, "max-width"), n = e(r, "display"); o.assert.notStrictEqual(a, "none", 'Try using the "max-width" style property '), o.assert.equal(n, "block", 'Use the "display" style property with a value of "block" for responsive images.'); var s = r.style.getPropertyValue("display"), i = r.style.getPropertyPriority("display"); r.style.setProperty("display", "none", "important"), t = e(r, "height"), r.style.setProperty("display", s, i), o.assert.equal(t, "auto", 'Use the "height" style property with a value of "auto" for responsive images.')
